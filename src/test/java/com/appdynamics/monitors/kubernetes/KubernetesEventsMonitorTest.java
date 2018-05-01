79cd285fe658fef6f4fbd61c1cc1182ab6542131
Michi <michi.eng@gmail.com>
2018-05-01 02:25:39 +0200

    more test coverage

--- src/test/java/com/appdynamics/monitors/kubernetes/KubernetesEventsMonitorTest.java
+++ src/test/java/com/appdynamics/monitors/kubernetes/KubernetesEventsMonitorTest.java
@@ -1,47 +1,87 @@
 package com.appdynamics.monitors.kubernetes;
 
-import com.google.gson.reflect.TypeToken;
+import com.appdynamics.extensions.conf.MonitorConfiguration;
+import com.appdynamics.extensions.util.MetricWriteHelper;
+import com.singularity.ee.agent.systemagent.api.TaskOutput;
+import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
 import io.kubernetes.client.ApiClient;
-import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
-import io.kubernetes.client.models.V1WatchEvent;
-import io.kubernetes.client.proto.V1;
+import io.kubernetes.client.models.V1EventList;
 import io.kubernetes.client.util.Config;
-import io.kubernetes.client.util.Watch;
 import org.junit.Test;
-
-import java.io.IOException;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.Assert.assertTrue;
 
 public class KubernetesEventsMonitorTest {
+
     @Test
-    public void testKubernetesEventsMonitor(){
-        ApiClient client = null;
+    public void testKubernetesEvents(){
+        Map<String, String> taskArgs = new HashMap<String, String>();
+        taskArgs.put("config-file", "src/test/resources/conf/integration-test-config.yml");
         try {
-            client = Config.defaultClient();
-        } catch (IOException e) {
+            testKubernetesEventsMonitorRun(taskArgs);
+        } catch (TaskExecutionException e) {
             e.printStackTrace();
         }
+    }
+
+    private void testKubernetesEventsMonitorRun(Map<String, String> taskArgs) throws TaskExecutionException {
+        TaskOutput result = new KubernetesEventsMonitor().execute(taskArgs, null);
+        assertTrue(result.getStatusMessage().contains("Finished executing"));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testKubernetesEventsMonitor() throws Exception {
+        MetricWriteHelper writer = Mockito.mock(MetricWriteHelper.class);
+        Runnable runner = Mockito.mock(Runnable.class);
+        MonitorConfiguration conf = new MonitorConfiguration("Custom Metrics|Kubernetes", runner, writer);
+        conf.setConfigYml("src/test/resources/conf/integration-test-config.yml");
+        Mockito.doAnswer(new Answer() {
+            public Object answer(InvocationOnMock invocationOnMock) {
+                Object[] args = invocationOnMock.getArguments();
+                System.out.println(args[0] + "=" + args[1]);
+                return null;
+            }
+        }).when(writer).printMetric(Mockito.anyString(), Mockito.any(BigDecimal.class), Mockito.anyString());
+        conf.setMetricWriter(writer);
+        Map<String, String> config = (Map<String, String>) conf.getConfigYml();
+
+        String apiKey = config.get("eventsApiKey");
+        String accountName = config.get("accountName");
+        URL publishUrl = Utilities.getUrl(config.get("eventsUrl") + "/events/publish/" + config.get("eventsSchemaName"));
+        URL schemaUrl = Utilities.getUrl(config.get("eventsUrl") + "/events/schema/" + "wrong2");
+        String requestBody = config.get("eventsSchemaDefinition");
+
+        if(EventsRestOperation.doRequest(schemaUrl, accountName, apiKey, "", "GET") == null){
+            EventsRestOperation.doRequest(schemaUrl, accountName, apiKey, requestBody, "POST");
+        }
+
+        ApiClient client = Config.fromConfig(config.get("kubeClientConfig"));
         Configuration.setDefaultApiClient(client);
         CoreV1Api api = new CoreV1Api();
 
-        try {
-            Watch<V1.Event> watchEvent = Watch.createWatch(client, api.listEventForAllNamespacesCall(null,
-                    null,
-                    false,
-                    null,
-                    10,
-                    null,
-                    null,
-                    20,
-                    true,
-                    null,
-                    null), new TypeToken<Watch.Response<V1WatchEvent>>() {}.getType());
-            for (Watch.Response<V1.Event> item : watchEvent) {
-                System.out.printf("%s : %s%n", item.type, item.object.getMetadata().getName());
-            }
-        } catch (ApiException e) {
-            e.printStackTrace();
+        V1EventList eventList = api.listEventForAllNamespaces(null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+        String payload = Utilities.createEventPayload(eventList).toString();
+
+        if(!payload.equals("[]")){
+            System.out.println(payload);
+            EventsRestOperation.doRequest(publishUrl, accountName, apiKey, payload, "POST");
         }
     }
 }
