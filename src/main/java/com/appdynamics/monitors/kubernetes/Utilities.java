3cfb75509025592cdd0edc934c6f829af6fd58c7
Michi <michi.eng@gmail.com>
2018-05-01 02:23:26 +0200

    create event logic

---
+++ src/main/java/com/appdynamics/monitors/kubernetes/Utilities.java
@@ -0,0 +1,73 @@
+package com.appdynamics.monitors.kubernetes;
+
+import com.appdynamics.monitors.kubernetes.config.Globals;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.kubernetes.client.models.V1Event;
+import io.kubernetes.client.models.V1EventList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+class Utilities {
+    private static final Logger logger = LoggerFactory.getLogger(Utilities.class);
+
+    static URL getUrl(String input){
+        URL url = null;
+        try {
+            url = new URL(input);
+        } catch (MalformedURLException e) {
+            logger.error("Error forming our from String {}", input, e);
+        }
+        return url;
+    }
+
+    static ArrayNode createEventPayload(V1EventList eventList) {
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode arrayNode = mapper.createArrayNode();
+        for (V1Event item : eventList.getItems()) {
+            Globals.lastElement = item.getMetadata().getSelfLink();
+            if (item.getMetadata().getSelfLink().equals(Globals.previousElement)){
+                arrayNode = mapper.createArrayNode();
+            }
+            else {
+                ObjectNode objectNode = mapper.createObjectNode();
+                objectNode = checkAddObject(objectNode, item.getFirstTimestamp(), "firstTimestamp");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getAnnotations(), "annotations");
+                objectNode = checkAddObject(objectNode, item.getLastTimestamp(), "lastTimestamp");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getCreationTimestamp(), "creationTimestamp");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getDeletionTimestamp(), "deletionTimestamp");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getFinalizers(), "finalizers");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getInitializers(), "initializers");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getLabels(), "labels");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getOwnerReferences(), "ownerReferences");
+                objectNode = checkAddObject(objectNode, item.getInvolvedObject().getKind(), "object_kind");
+                objectNode = checkAddObject(objectNode, item.getInvolvedObject().getName(), "object_name");
+                objectNode = checkAddObject(objectNode, item.getInvolvedObject().getNamespace(), "object_namespace");
+                objectNode = checkAddObject(objectNode, item.getInvolvedObject().getResourceVersion(), "object_resourceVersion");
+                objectNode = checkAddObject(objectNode, item.getInvolvedObject().getUid(), "object_uid");
+                objectNode = checkAddObject(objectNode, item.getMessage(), "message");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getClusterName(), "clusterName");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getGenerateName(), "generateName");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getGeneration(), "generation");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getName(), "name");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getNamespace(), "namespace");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getResourceVersion(), "resourceVersion");
+                objectNode = checkAddObject(objectNode, item.getMetadata().getSelfLink(), "selfLink");
+                arrayNode.add(objectNode);
+            }
+        }
+        Globals.previousElement = Globals.lastElement;
+
+        return arrayNode;
+    }
+
+    private static ObjectNode checkAddObject(ObjectNode objectNode, Object object, String fieldName){
+        if(object != null){
+            objectNode.put(fieldName, object.toString());
+        }
+        return objectNode;
+    }
+}
